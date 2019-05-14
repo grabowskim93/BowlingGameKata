151de0f610363cac169a6055aef769d4c130a09e
Mateusz Grabowski <grabowski.m93@gmail.com>
2019-05-14 11:48:23 +0200

    #10 RED - test split rolls into frames

--- tests/BowlingGameTest.php
+++ tests/BowlingGameTest.php
@@ -35,12 +35,13 @@ class BowlingGameTest extends TestCase
         yield 'Valid data#1' => ['input' => [5, 5], 'output' => 10, 'expectException' => false];
         yield 'Valid data#2' => ['input' => [10], 'output' => 10, 'expectException' => false];
         yield 'Valid data#3' => ['input' => [0], 'output' => 0, 'expectException' => false];
-        yield 'Valid data#4' => ['input' => [6, 5], 'output' => null, 'expectException' => DomainException::class];
+        yield 'Valid data#4' => ['input' => [5,4,3,2], 'output' => 14, 'expectException' => false];
         yield 'Invalid data#1' => [
             'input' => [11], 'output' => null, 'expectException' => InvalidArgumentException::class
         ];
         yield 'Invalid data#2' => [
             'input' => [-1], 'output' => null, 'expectException' => InvalidArgumentException::class
         ];
+        yield 'Invalid data#3' => ['input' => [6, 5], 'output' => null, 'expectException' => DomainException::class];
     }
 }
