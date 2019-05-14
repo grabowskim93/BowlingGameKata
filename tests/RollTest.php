9bf99b04a2a38323e97c52cbd7f60f28358c6bee
Mateusz Grabowski <grabowski.m93@gmail.com>
2019-05-14 13:20:56 +0200

    #12 RED - test roll pins

---
+++ tests/RollTest.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Test;
+
+use App\Roll;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+class RollTest extends TestCase
+{
+    public function testRoll()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Roll(11);
+        $this->expectException(InvalidArgumentException::class);
+        new Roll(-1);
+    }
+}
