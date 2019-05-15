dc6f4e1edc873d57321360be9d1537b4d318539b
Mateusz Grabowski <grabowski.m93@gmail.com>
2019-05-15 09:06:49 +0200

    #1 RED - test bowling roll

--- tests/BowlingGameTest.php
+++ tests/BowlingGameTest.php
@@ -2,8 +2,16 @@
 
 namespace Test;
 
+use App\BowlingGame;
 use PHPUnit\Framework\TestCase;
 
 class BowlingGameTest extends TestCase
 {
+    public function testRoll()
+    {
+        $bowlingGame = new BowlingGame();
+        $bowlingGame->roll();
+
+        $this->assertTrue(true);
+    }
 }
