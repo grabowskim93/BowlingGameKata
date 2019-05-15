31f26ba4aff06cdfdc56561b798036f46bb9dd22
Mateusz Grabowski <grabowski.m93@gmail.com>
2019-05-15 10:09:20 +0200

    #3 REFACTOR - add comments, add constants
    Move spare logic to private method

--- src/BowlingGame.php
+++ src/BowlingGame.php
@@ -14,11 +14,22 @@ namespace App;
 class BowlingGame
 {
     /**
+     * Amount of pins required for spare.
+     *
+     * @var int
+     */
+    const SPARE_SUM = 10;
+
+    /**
+     * Array with all rolls pins.
+     *
      * @var array
      */
     private $pins;
 
     /**
+     * Total score of game.
+     *
      * @var int
      */
     private $score;
@@ -39,14 +50,7 @@ class BowlingGame
         $this->score += $pins;
 
         $this->pins[] = $pins;
-
-        $currentRoll = count($this->pins) - 1; //because increments from 0.
-        if (isset($this->pins[$currentRoll-1])
-            && isset($this->pins[$currentRoll-2])
-            && $this->pins[$currentRoll-1] + $this->pins[$currentRoll-2] === 10
-        ) {
-            $this->score += $pins;
-        }
+        $this->scoreSpare($pins);
     }
 
     /**
@@ -57,5 +61,21 @@ class BowlingGame
     public function score(): int
     {
         return $this->score;
+    }
+
+    /**
+     * Add extra points if spare.
+     *
+     * @param int $pins
+     */
+    private function scoreSpare(int $pins): void
+    {
+        $currentRoll = count($this->pins) - 1; //because increments from 0.
+        if (isset($this->pins[$currentRoll-1])
+            && isset($this->pins[$currentRoll-2])
+            && $this->pins[$currentRoll-1] + $this->pins[$currentRoll-2] === self::SPARE_SUM
+        ) {
+            $this->score += $pins;
+        }
     }
 }
