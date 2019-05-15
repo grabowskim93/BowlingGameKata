cf35010c2f2cd32bfa06a7e02ab4c0b097fb8e72
Mateusz Grabowski <grabowski.m93@gmail.com>
2019-05-15 09:45:04 +0200

    #3 RED - test spare in first frame

--- tests/BowlingGameTest.php
+++ tests/BowlingGameTest.php
@@ -7,6 +7,7 @@
 namespace Test;
 
 use App\BowlingGame;
+use Generator;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -18,16 +19,40 @@ class BowlingGameTest extends TestCase
 {
     /**
      * Test bowling roll.
+     *
+     * @dataProvider rollsProvider
+     *
+     * @param $input
+     * @param $output
+     * @param $expectException
      */
-    public function testRoll()
+    public function testRoll($input, $output, $expectException)
     {
         $bowlingGame = new BowlingGame();
 
-        $rolls = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-        foreach ($rolls as $roll) {
+        foreach ($input as $roll) {
             $bowlingGame->roll($roll);
         }
 
-        $this->assertEquals(20, $bowlingGame->score());
+        $this->assertEquals($output, $bowlingGame->score());
+    }
+
+    /**
+     * Input - rolls in bowling game.
+     * Output - score after game.
+     * ExpectedError - determines if non valid game.
+     *
+     * @return Generator
+     */
+    public function rollsProvider()
+    {
+        yield 'Game#1 - all ones'   => [
+            'input' => [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            'output' => 20, 'expectError' => false
+        ];
+        yield 'Game#1 - spare in first frame'  => [
+            'input' => [6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            'output' => 16, 'expectError' => false
+        ];
     }
 }
